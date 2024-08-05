43a7d6013f255544ade8248055db43b5fbf16c11
Raquel <raquelviena124@gmail.com>
2024-08-05 05:13:16 -0300

    firstcommit

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+/*
+	Verti by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			speed: 300
+		});
+
+	// Nav.
+
+		// Toggle.
+			$(
+				'<div id="navToggle">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
+
+const scrollToSection1Button = document.getElementById('scrollboutme');
+const scrollToSection2Button = document.getElementById('scrollskills');
+const section1 = document.getElementById('features-wrapper');
+const section2 = document.getElementById('main-wrapper');
+
+// Função para rolar até a seção
+function scrollToSection(element) {
+  element.scrollIntoView({
+	behavior: 'smooth', // Para rolagem suave
+	block: 'start' // Posicionar no início da seção
+  });
+}
+
+// Adicionar event listeners aos botões
+scrollToSection1Button.addEventListener('click', () => scrollToSection(section1));
+scrollToSection2Button.addEventListener('click', () => scrollToSection(section2));
+
